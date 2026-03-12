8310f46b69e854d43bcdda8fbd408e454967d78f
conorg763 <conor@nigma.co.uk>
2026-03-12 09:17:34 -0700

    added updates

---
+++ WebApplicationAPI/Models/Author.cs
@@ -0,0 +1,7 @@
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Country { get; set; }
+}
--- WebApplicationAPI/Program.cs
+++ WebApplicationAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Data.SqlClient;
+using Dapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +9,26 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddTransient<SqlConnection>(_ =>
+    new SqlConnection(builder.Configuration.GetConnectionString("BookshopDB")));
+
+
+//Temporary connection test — remove after verifying - above builder.build()
+using var testConnection = new SqlConnection(
+builder.Configuration.GetConnectionString("BookshopDB"));
+
+// Before builder.Build()
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-// Temporary connection test — remove after verifying
-using var testConnection = new SqlConnection(
-    builder.Configuration.GetConnectionString("BookshopDB"));
+// After builder.Build()
+app.UseSwagger();
+app.UseSwaggerUI();
 
-try
+
+trydotnet rund
 {
     await testConnection.OpenAsync();
     Console.WriteLine("Database connection successful");
@@ -23,6 +37,15 @@ catch (Exception ex)
 {
     Console.WriteLine($"Connection failed: {ex.Message}");
 }
+
+
+// Task 1: Get all Authors 
+
+app.MapGet("/api/authors", async (SqlConnection db) =>
+{
+    var authors = await db.QueryAsync<Author>("SELECT * FROM Authors");
+    return Results.Ok(authors);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
